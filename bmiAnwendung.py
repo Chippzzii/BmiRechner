a02e04c92f45d207713e24f6b1d98301306e9baa
c.hülsmann <115979110+Chippzzii@users.noreply.github.com>
2023-10-25 13:14:02 +0200

    first commit

---
+++ bmiAnwendung.py
@@ -0,0 +1,41 @@
+from bmiGui import Ui_BMIFenster
+from PyQt6 import QtWidgets, QtGui, QtCore
+
+import sys
+
+
+class bmiAnwendung(QtWidgets.QMainWindow, Ui_BMIFenster):
+    def __init__(self, parent=None):
+        super(bmiAnwendung, self).__init__(parent=parent)
+        self.setupUi(self)
+
+    def btn_click(self):
+        bmiAnwendung.bmiBerechnen(self)
+        self.labelBMAusgabe.setText(str(bmiAnwendung.bmiBerechnen(self)))
+        bmiAnwendung.updateBild(self, bmiAnwendung.bmiBerechnen(self))
+        pass
+
+    def bmiBerechnen(self):
+        gewicht = self.eingabeGewicht.value()
+        groesse = self.eingabeGroesse.value() / 100
+        bmi = gewicht / (groesse * groesse)
+        bmi = round(bmi, 2)
+        return bmi
+
+
+    def updateBild(self, bmi):
+        if bmi < 18.5:
+            self.labelBild.setPixmap(QtGui.QPixmap("bmiRessourcen/Grenbone.png"))
+        elif 18.5 <= bmi < 24.9:
+            self.labelBild.setPixmap(QtGui.QPixmap("bmiRessourcen/Grenbone.png"))
+        elif 25 <= bmi < 29.9:
+            self.labelBild.setPixmap(QtGui.QPixmap("bmiRessourcen/Lama.png"))
+        else:
+            self.labelBild.setPixmap(QtGui.QPixmap("bmiRessourcen/test.png"))
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    form = bmiAnwendung()
+    form.show()
+    sys.exit(app.exec())
